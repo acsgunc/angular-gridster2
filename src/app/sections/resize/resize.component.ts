8ab8e1b700b306afdea125795b598f69909a1b08
acsgunc <acs.gunc@gmail.com>
2024-04-28 14:04:13 +0800

    resize

--- src/app/sections/resize/resize.component.ts
+++ src/app/sections/resize/resize.component.ts
@@ -105,17 +105,8 @@ export class ResizeComponent implements OnInit {
     };
 
     this.dashboard = [
-      { cols: 2, rows: 1, y: 0, x: 0 },
-      { cols: 2, rows: 2, y: 0, x: 2 },
-      { cols: 1, rows: 1, y: 0, x: 4 },
-      { cols: 3, rows: 2, y: 1, x: 4 },
-      { cols: 1, rows: 1, y: 4, x: 5 },
-      { cols: 1, rows: 1, y: 2, x: 1 },
-      { cols: 2, rows: 2, y: 5, x: 5 },
-      { cols: 2, rows: 2, y: 3, x: 2 },
-      { cols: 2, rows: 1, y: 2, x: 2 },
-      { cols: 1, rows: 1, y: 3, x: 4 },
-      { cols: 1, rows: 1, y: 0, x: 6 }
+      { cols: 2, rows: 2, y: 0, x: 0 },
+      { cols: 2, rows: 2, y: 0, x: 2 }
     ];
   }
 
